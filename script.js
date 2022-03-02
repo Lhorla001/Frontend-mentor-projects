322eda37dfaefa3c0223249a4292eb9906cd9f20
Titilola Shittu <teteelola@gmail.com>
2022-03-02 16:03:17 +0100

    Create script.js
    first commit

---
+++ script.js
@@ -0,0 +1,27 @@
+const projects = [
+	{
+		name: 'nft-preview-card-component-main',
+	}
+]
+
+const list = document.getElementById('list');
+
+projects.forEach(({ name }, i) => {
+	const listItem = document.createElement('li');
+
+	listItem.innerHTML = `
+		<a href="/${name}/index.html">
+			<img src="/${name}/design/desktop-design.jpg" alt="${name}" />
+			<p>${i + 1}. ${formatProjectName(name)}</p>
+		</a>
+	`;
+
+	list.appendChild(listItem);
+});
+
+function formatProjectName(name) {
+	return name
+		.split('-')
+		.map(word => word[0].toUpperCase() + word.slice(1))
+		.join(' ');
+}
